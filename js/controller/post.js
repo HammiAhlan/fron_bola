6f61bec51244d068abf0b48dd1c648daf8312ed2
HammiAhlan <hammiahlan@gmail.com>
2024-07-16 22:10:08 +0700

    tambah post

--- js/controller/post.js
+++ js/controller/post.js
@@ -1,28 +1,33 @@
-import { postData } from "https://bukulapak.github.io/api/process.js";
-import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse} from "../config/url_post.js";
+import { urlPOST, AmbilResponse } from '../config/url_post.js';
 
+export function insertData() {
+    const data = {
+        Nama_Pemain: document.getElementById('name').value,
+        Tim: {
+            Nama_Club: document.getElementById('club').value,
+            Liga: document.getElementById('liga').value,
+            Tahun_Berdiri: parseInt(document.getElementById('tahun').value),
+            Stadion: document.getElementById('stadion').value,
+            Manajer: document.getElementById('manajer').value,
+            Logo: document.getElementById('logo').value,
+            Jumlah_Pemain: 0 // Sesuaikan dengan input yang sesuai jika ada
+        },
+        Posisi: document.getElementById('posisi').value,
+        Tinggi: parseInt(document.getElementById('tinggi').value),
+        Berat: parseInt(document.getElementById('berat').value),
+        Tanggal_Lahir: document.getElementById('tanggal_lahir').value,
+        Negara: document.getElementById('negara').value,
+        No_Punggung: parseInt(document.getElementById('no_punggung').value)
+    };
 
-function pushData(){
-    var namapemain = getValue("namapemain");
-
-    let data = {
-        namapemain : getValue("namapemain"),
-        tim : {
-            namapemain : getValue("namapemain"),
-            nama_club : getValue("nama_club"),
-            liga : [{
-                tahun_berdiri : parseInt(getValue("tahun_berdiri")),
-                stadion : getValue("stadion"),
-                manajer : getValue("manajer"),
-                logo : getValue(""),
-
-            }],
-            namapemain : namapemain.split(",")
-        }
-    }
-    postData(urlPOST, data, AmbilResponse);
-
+    fetch(urlPOST, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => AmbilResponse(result))
+    .catch(error => console.error('Error:', error));
 }
-
-onClick("button", pushData);
